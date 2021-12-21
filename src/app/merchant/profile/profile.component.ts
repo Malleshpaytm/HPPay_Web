a6dd93a1dadc32289268322483f4360c6f4e26e9
Siddhant.Patra <siddhant.patra@wisetechglobal.com>
2021-12-21 23:35:09 +0530

    merchant update complete

--- src/app/merchant/profile/profile.component.ts
+++ src/app/merchant/profile/profile.component.ts
@@ -12,6 +12,7 @@ import { SalesArea } from 'src/app/models/sales-area';
 import { MerchantType } from 'src/app/models/merchant-type';
 import { District } from 'src/app/models/district';
 import { RegionalOffice } from 'src/app/models/regional-office';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -27,6 +28,7 @@ export class ProfileComponent implements OnInit {
   showSubSubMenu: boolean = false;
 
   merchantDetails: MerchantDetails = new MerchantDetails();
+  displayMerchantDetails: MerchantDetails = new MerchantDetails();
   cityList: City[];
   stateList: State[];
   zonalOfficeList: Zone[];
@@ -34,15 +36,6 @@ export class ProfileComponent implements OnInit {
   merchantTypeList: MerchantType[];
   districtList: District[];
   regionalOfficeList: RegionalOffice[];
-
-  cities = [
-    { label: 'Select City', value: null },
-    { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
-    { label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } },
-    { label: 'London', value: { id: 3, name: 'London', code: 'LDN' } },
-    { label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } },
-    { label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } },
-  ];
 
   constructor(
     private merchantService: MerchantService,
@@ -57,14 +50,60 @@ export class ProfileComponent implements OnInit {
         this.merchantDetails = details;
       });
     this.merchantDetails = await this.getMerchantDetailsAsync();
+    this.displayMerchantDetails = { ...this.merchantDetails };
 
     await this.populateOptionsAsync();
-    this.populateMerchantWithOptionCodes();
+    setTimeout(() => {
+      this.populateMerchantWithOptionCodes();
+    }, 0);
   }
 
   populateMerchantWithOptionCodes() {
+    if (this.merchantDetails.perm_City) {
+      this.displayMerchantDetails.perm_City = this.cityList.find(
+        (city) => this.merchantDetails.perm_City === city.city_Name
+      )?.cityCode;
+    }
 
-    this.merchantDetails.perm_City = this.cityList.find()
+    if (this.merchantDetails.perm_State) {
+      this.displayMerchantDetails.perm_State = this.stateList.find(
+        (state) => this.merchantDetails.perm_State === state.state_Name
+      )?.stateCode;
+    }
+
+    if (this.merchantDetails.merchant_Type_Id) {
+      this.displayMerchantDetails.merchant_Type_Id = this.merchantTypeList.find(
+        (mcType) =>
+          this.merchantDetails.merchant_Type_Id === mcType.merchant_Type_Name
+      )?.merchant_Type_Id;
+    }
+
+    if (this.merchantDetails.regional_Office) {
+      this.displayMerchantDetails.regional_Office =
+        this.regionalOfficeList.find(
+          (ro) => this.merchantDetails.regional_Office === ro.rO_Name
+        )?.rO_Code;
+    }
+
+    if (this.merchantDetails.zonal_Office) {
+      this.displayMerchantDetails.zonal_Office = this.zonalOfficeList.find(
+        (zonalOfc) => this.merchantDetails.zonal_Office === zonalOfc.zone_Name
+      )?.zone_Code;
+    }
+
+    if (this.merchantDetails.salesArea) {
+      this.displayMerchantDetails.salesArea = this.salesAreaList.find(
+        (salesArea) =>
+          this.merchantDetails.salesArea === salesArea.district_Name
+      )?.district_Code;
+    }
+
+    if (this.merchantDetails.perm_District) {
+      this.displayMerchantDetails.perm_District = this.districtList.find(
+        (district) =>
+          this.merchantDetails.perm_District === district.district_Name
+      )?.district_Code;
+    }
   }
 
   async getMerchantDetailsAsync(): Promise<MerchantDetails> {
@@ -76,30 +115,37 @@ export class ProfileComponent implements OnInit {
   async populateOptionsAsync(): Promise<boolean> {
     let cityPromise = this.adminService
       .getCity(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let statePromise = this.adminService
       .getStates(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let merchantTypePromise = this.adminService
       .getMerchantType(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let regionalOfficePromise = this.adminService
       .getRegionalOffice(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let zonalOfficePromise = this.adminService
       .getZonalOffice(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let salesAreaPromise = this.adminService
       .getSalesArea(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     let districtPromise = this.adminService
       .getDistrict(MerchantHelper.addMerchantMetadataToModels({}))
+      .pipe(map((resp: any) => resp.data))
       .toPromise();
 
     this.cityList = await cityPromise;
@@ -113,9 +159,12 @@ export class ProfileComponent implements OnInit {
   }
 
   handleSaveDetails() {
+    this.displayMerchantDetails.comm_District = this.displayMerchantDetails.comm_District || '';
+    this.displayMerchantDetails.comm_City = this.displayMerchantDetails.comm_City || '';
+    this.displayMerchantDetails.comm_State = this.displayMerchantDetails.comm_State || '';
     this.merchantService
       .updateMerchantDetails(
-        MerchantHelper.addMerchantMetadataToModels(this.merchantDetails)
+        MerchantHelper.addMerchantMetadataToModels(this.displayMerchantDetails)
       )
       .subscribe((resp) => {
         if (resp.success) {
